8f307430fa848da4ee0280ff7011b2e963027ede
Bruno Fenzl <bruno@brunofenzl.com>
2016-08-25 15:08:02 +0200

    loads o changes

--- _includes/scripts/main.js
+++ _includes/scripts/main.js
@@ -8,6 +8,32 @@
 			evt.preventDefault();
 			$('body').toggleClass('open-right');
 		});
+
+
+		var $toggleListView = $('.toggle-list-view');
+
+		$toggleListView.on('click', function( evt ){
+			console.log('toggle-list-view click');
+			evt.preventDefault();
+
+			//toggle it's own state
+			$toggleListView.toggleClass('grid');
+
+			//toggle internal style of thumbs 
+			$('.product-list, .cmp-product-thumb').toggleClass('grid');
+			
+			// //toggle thumbs size
+			// if( $('.product-list').hasClass('grid') ){
+
+			// 	$('.product-list').removeClass('grid').addClass('rows');
+			
+			// }else if( $('.product-list').hasClass('rows') ){
+			
+			// 	$('.product-list').removeClass('rows').addClass('grid');
+			
+			// }
+		});
+
 	}
 
 	window.CallistoMain = CallistoMain;
